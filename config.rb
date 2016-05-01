7b425bd9982dd93209cd2c69db8e594e22252ecb
Konstantin Komelin <konstantin.komelin@gmail.com>
2016-05-01 08:43:45 +0300

    Implemented the first draft.

---
+++ config.rb
@@ -0,0 +1,24 @@
+# Require any additional compass plugins here.
+require 'susy'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "images"
+generated_images_dir = images_dir + "/generated"
+javascripts_dir = "js"
+relative_assets = true
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
